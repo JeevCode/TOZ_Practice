04e7fc522ce2cf37bea410b584b05453a9341d75
Mariyam Bee Bahauddin <mariyam@talentoz.com>
2024-04-16 00:58:44 +0530

    git commit -m "2nd commit"

--- TALENTOZ/ROOT/POM/features/Log_In/stepDefinition/login.step.ts
+++ TALENTOZ/ROOT/POM/features/Log_In/stepDefinition/login.step.ts
@@ -1,17 +1,22 @@
 //import { Given } from "@wdio/cucumber-framework";
 
-import { Given, Then } from "@wdio/cucumber-framework";
+import { Given, Then,When } from "@wdio/cucumber-framework";
 import loginPage from "../pageObject/LogIn.page.js";
 
 
-Given(/^the user has opened the browser and naivgate to the page$/,async function (){
-    //await LogInPage.open();
-      await loginPage.open();
-      await browser.maximizeWindow();
-    
-    const mailid=await $("//input[@id='email']");
-    await mailid.setValue("mariyam@deel.com");
-    const enterImg = await $("#enterimg");
-    await enterImg.click();
-    await browser.pause(7000);
+Given(/^the user has opened the browser and naivgate to the page$/, async function () {
+
+  await loginPage.open();
+  await browser.maximizeWindow();
+});
+
+When(/^i enter the mailID in the feild$/,async function () {
+  const entermailid = await $("//input[@id='email']"); 
+  await entermailid.setValue("mariyam@deel.com");
+});
+
+Then(/^i can see the signup page$/, async function () {
+  const Login = await $("#enterimg");
+  await Login.click();
+  await browser.pause(7000);
 });
--- TALENTOZ/ROOT/POM/features/masterXPaths.page.ts
+++ TALENTOZ/ROOT/POM/features/masterXPaths.page.ts
@@ -5,8 +5,11 @@ import Page from "./Log_In/pageObject/page.js";
  * sub page containing specific selectors and methods for a specific page
  */
 class masterXPaths extends Page {
+   getmailid()
+   {
+    return $("//input[@id='email']");
+   }  
    
-  
 }
 
 export default new masterXPaths();
