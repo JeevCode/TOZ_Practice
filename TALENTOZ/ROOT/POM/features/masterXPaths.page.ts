cebf01f13158a33406b0b7855b7d48b51ab592cf
Jeeva Subramanian <jeeva.subramanian@deel.com>
2024-04-17 17:41:51 +0530

    First SignIn Feature

--- TALENTOZ/ROOT/POM/features/masterXPaths.page.ts
+++ TALENTOZ/ROOT/POM/features/masterXPaths.page.ts
@@ -6,7 +6,11 @@ import Page from "./Log_In/pageObject/page.js";
  */
 class masterXPaths extends Page {
   
-  
+  public async EmpTap_LableWithInput(Lable:string, ActionType:string) {
+    return $(`//div[contains(@id,'hdr_container_New_Emp_Tab')]//div[contains(@class,'grid-stack-item')][.//*[contains(.,'${Lable}')]]//${ActionType}`)
+  }
+
+
 }
 
 export default new masterXPaths();
